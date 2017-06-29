aea7a8d91f97d7f081245cc198a9cea6d843857a
cdknudsen8 <cdknusen@yahoo.com>
2017-06-29 09:37:05 -0700

    testing sublime

--- code/hackscan_parser.py
+++ code/hackscan_parser.py
@@ -11,6 +11,7 @@ from datetime import *
 from tqdm import tqdm
 import time
 
+#read in the manually run data file
 data = read_csv('hackscanout.txt',
                      delimiter=',',
                      usecols = (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31),
